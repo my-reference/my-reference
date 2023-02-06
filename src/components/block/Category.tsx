57c81bf14d0edf4d7a40478f7edcbc9e63a705fd
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-02-06 20:40:22 +0900

    feat: main category

--- src/components/block/Category.tsx
+++ src/components/block/Category.tsx
@@ -1,6 +1,63 @@
 import React from 'react';
+import styled from 'styled-components';
+import CategoryBtn from '../atoms/CategoryBtn';
 
-function Category() {
-	return <div></div>;
+const CategoryWrapper = styled.div``;
+
+const CategoryInner = styled.div`
+	padding: 0 30px;
+`;
+
+const CategoryBlock = styled.div`
+	margin-bottom: 25px;
+`;
+
+const CategoryTitle = styled.h3`
+	font-size: 18px;
+	font-weight: 600;
+	color: #18191b;
+`;
+
+const CategoryListWrap = styled.ul`
+	margin: 0;
+	padding: 0;
+`;
+
+const CategoryList = styled.li`
+	list-style: none;
+`;
+
+type CategoryType = {
+	favorites: Array<string>;
+	categories: Array<string>;
+};
+
+function Category({ favorites, categories }: CategoryType) {
+	return (
+		<CategoryWrapper>
+			<CategoryInner>
+				<CategoryBlock>
+					<CategoryTitle>📌 즐겨찾기</CategoryTitle>
+					<CategoryListWrap>
+						{favorites.map((favoritesCategoryName) => (
+							<CategoryList>
+								<CategoryBtn categoryName={favoritesCategoryName} />
+							</CategoryList>
+						))}
+					</CategoryListWrap>
+				</CategoryBlock>
+				<CategoryBlock>
+					<CategoryTitle>📂 카테고리</CategoryTitle>
+					<CategoryListWrap>
+						{categories.map((categoryName) => (
+							<CategoryList>
+								<CategoryBtn categoryName={categoryName} />
+							</CategoryList>
+						))}
+					</CategoryListWrap>
+				</CategoryBlock>
+			</CategoryInner>
+		</CategoryWrapper>
+	);
 }
 export default Category;
