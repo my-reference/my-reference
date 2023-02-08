59b4223a9bb6a124bcf9fb38410469fdc988168d
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-02-08 18:26:36 +0900

    feat: main 컴포넌트 + mock data 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/block/Header';
+import Main from './components/pages/Main';
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
+			<Main />
 		</div>
 	);
 }
--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+import mockMainData from '../../data/mockMainData';
+import Category from '../block/Category';
+import Posts from '../block/Posts';
+
+const MainPageStyle = styled.div`
+	display: flex;
+	padding: 60px 20px;
+`;
+
+const mockData = mockMainData;
 
 function Main() {
 	return (
-		<div>
-			<div>ss</div>
-		</div>
+		<MainPageStyle>
+			<div>
+				<Category favorites={mockData.favoritesCategories} categories={mockData.categories} />
+			</div>
+			<div>
+				<Posts categoryName={mockData.CATEGORY_NAME} posts={mockData.posts} />
+			</div>
+		</MainPageStyle>
 	);
 }
 
